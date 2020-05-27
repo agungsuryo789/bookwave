483e9af3ebf4417a8aed4c5bbf8da9d4686bfc34
Agung Suryo Sundoro <agungsuryo645@gmail.com>
2020-05-27 20:33:19 +0700

    Read Book AHA Page
    Create DOM Manipulation Font Size
    -----------------//---------- Background Color
    Create Chapter Navigation (Outline)
    Create Fitur DOM Manipulation highlight text and save highlight

--- src/views/main/BookChapter.vue
+++ src/views/main/BookChapter.vue
@@ -1,63 +1,188 @@
 <template>
-  <div>
+  <div :class="{ bgWhite: bgWhite, bgGrey: bgGrey, bgBlack: bgBlack }">
     <v-container fluid>
       <v-row>
-        <v-col class="d-flex flex-row justify-end">
-          <v-btn @click="showTools = !showTools" depressed>v</v-btn>
+        <v-col class="d-flex flex-row justify-space-between">
+          <v-btn
+            depressed
+            :class="{ bgWhite: bgWhite, bgGrey: bgGrey, bgBlack: bgBlack }"
+            @click="hasHistory() ? $router.go(-1) : $router.push('/')"
+          >
+            <v-icon>mdi-less-than</v-icon>
+          </v-btn>
+          <v-btn
+            @click="showMenuTop = !showMenuTop, showTools = false"
+            depressed
+            :class="{ bgWhite: bgWhite, bgGrey: bgGrey, bgBlack: bgBlack }"
+          >
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
         </v-col>
       </v-row>
-      <v-row v-if="showTools">
-        <v-col>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex doloribus magnam nostrum ea! Quos voluptates ratione quisquam quod, eum et exercitationem dolores quaerat consequuntur repellendus accusantium doloremque similique officiis accusamus.</v-col>
-      </v-row>
-    </v-container>
-    <v-container v-for="book in bookDetail.data" :key="book.id_buku">
-      <v-carousel height="300" hide-delimiters touchless :show-arrows="false">
-        <v-carousel-item>
-          <v-sheet color="white" height="100%" style="color:black;">
-            <v-row class="fill-height" align="center" justify="center">
-              <div
-                class="display-3 font-weight-bold px-5"
-              >{{ book.data_chapter[chapterArray].judul_chapter }}</div>
-            </v-row>
-          </v-sheet>
-        </v-carousel-item>
-      </v-carousel>
-      <v-row>
-        <v-col style="position:relative;">
-          <p class="headline">{{ book.data_chapter[chapterArray].isi_chapter }}</p>
-          <div class="d-flex flex-row justify-space-between mt-12 mb-2">
-            <v-btn
-              v-if="chapterArray > 0"
-              @click="chapterArray -= 1"
-              depressed
-              rounded
-              style="position:absolute;bottom:0;left:0;margin:0 10px;"
-            >prev</v-btn>
-            <v-btn depressed style="position:absolute;bottom:0;left:45%;">{{ chapterArray + 1 }}</v-btn>
-            <v-btn
-              v-if="chapterArray < parseInt(book.data_chapter.length) - 1"
-              @click="chapterArray += 1"
-              depressed
-              rounded
-              style="position:absolute;bottom:0;right:0;margin:0 10px;"
-            >next</v-btn>
+      <v-row v-if="showMenuTop">
+        <v-col style="max-width:800px;margin:0 auto;">
+          <div class="d-flex flex-row justify-center align-center">
+            <div class="d-flex flex-column justify-center align-center mx-4">
+              <v-btn @click="showTools = true, showMenuTop = false" depressed outlined>
+                <v-icon>mdi-format-color-text</v-icon>
+              </v-btn>
+              <p>Teks</p>
+            </div>
+            <div class="d-flex flex-column justify-center align-center mx-4">
+              <v-btn @click="outlineMenu = !outlineMenu" depressed text>
+                <v-icon>mdi-format-list-bulleted</v-icon>
+              </v-btn>
+              <p>Outlined</p>
+            </div>
+            <div class="d-flex flex-column justify-center align-center mx-4">
+              <v-btn @click="showMenuTop = false" depressed text>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+              <p>Share</p>
+            </div>
           </div>
         </v-col>
       </v-row>
-      <div class="mt-5" style="position:sticky;bottom:0;left:0;width:100%;">
-        <div class="d-flex flex-column">
-          <vuetify-audio
-            :file="book.data_chapter[chapterArray].audio_chapter"
-            :autoPlay="false"
-            color="success"
-          ></vuetify-audio>
-        </div>
-      </div>
+      <v-row v-if="showTools">
+        <v-col style="max-width:800px;margin:0 auto;">
+          <div class="d-flex flex-row justify-center align-center">
+            <v-btn
+              class="font-weight-bold"
+              tile
+              depressed
+              outlined
+              ripple
+              @click="bgWhite = !bgWhite"
+              width="150"
+              style="border:2px solid #ededed;text-transform:none;background-color:white;"
+            >Aa</v-btn>
+            <v-btn
+              class="font-weight-bold"
+              tile
+              depressed
+              outlined
+              ripple
+              @click="bgGrey = !bgGrey"
+              width="150"
+              style="border:2px solid #ededed;text-transform:none;background-color:#E5E5E5;"
+            >Aa</v-btn>
+            <v-btn
+              class="font-weight-bold"
+              tile
+              depressed
+              outlined
+              ripple
+              @click="bgBlack = !bgBlack"
+              width="150"
+              color="white"
+              style="border:2px solid #ededed;text-transform:none;background-color:black;"
+            >Aa</v-btn>
+          </div>
+          <div class="d-flex flex-row my-3" style="width:450px;margin:0 auto;">
+            <span>Aa</span>
+            <v-slider v-model="slider" class="align-center" :max="max" :min="min" thumb-label></v-slider>
+            <span class="font-weight-bold headline">Aa</span>
+          </div>
+        </v-col>
+      </v-row>
     </v-container>
+    <template v-if="!outlineMenu">
+      <v-container v-for="book in bookDetail.data" :key="book.id_buku">
+        <v-carousel height="150" hide-delimiters touchless :show-arrows="false">
+          <v-carousel-item>
+            <v-sheet
+              :class="{ bgWhite: bgWhite, bgGrey: bgGrey, bgBlack: bgBlack }"
+              color="white"
+              height="100%"
+              style="color:black;"
+            >
+              <v-row class="fill-height" align="center" justify="center">
+                <div
+                  class="display-3 font-weight-bold px-5"
+                >{{ book.data_chapter[chapterArray].judul_chapter }}</div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+        <v-row>
+          <v-col style="position:relative;">
+            <p id="chapterText" :style="styleObject" @mouseup="tooltipShow = true">
+              {{ book.data_chapter[chapterArray].isi_chapter }}
+              <span id="tooltip">
+                <v-btn>Warnai</v-btn>
+				<v-btn>Highlight</v-btn>
+              </span>
+            </p>
+            <div class="d-flex flex-row justify-space-between mt-12 mb-2">
+              <v-btn
+                :class="{ bgWhite: bgWhite, bgGrey: bgGrey, bgBlack: bgBlack }"
+                v-if="chapterArray > 0"
+                @click="chapterArray -= 1"
+                depressed
+                rounded
+                style="position:absolute;bottom:0;left:0;margin:0 10px;"
+              >
+                <v-icon>mdi-less-than</v-icon>
+                <v-icon>mdi-less-than</v-icon>
+              </v-btn>
+              <v-btn
+                :class="{ bgWhite: bgWhite, bgGrey: bgGrey, bgBlack: bgBlack }"
+                depressed
+                style="position:absolute;bottom:0;left:45%;"
+              >{{ chapterArray + 1 }}</v-btn>
+              <v-btn
+                :class="{ bgWhite: bgWhite, bgGrey: bgGrey, bgBlack: bgBlack }"
+                v-if="chapterArray < parseInt(book.data_chapter.length) - 1"
+                @click="chapterArray += 1"
+                depressed
+                rounded
+                style="position:absolute;bottom:0;right:0;margin:0 10px;"
+              >
+                <v-icon>mdi-greater-than</v-icon>
+                <v-icon>mdi-greater-than</v-icon>
+              </v-btn>
+            </div>
+          </v-col>
+        </v-row>
+        <div class="mt-10" style="position:sticky;bottom:0;left:0;width:100%;">
+          <div class="d-flex flex-column">
+            <vuetify-audio
+              :file="book.data_chapter[chapterArray].audio_chapter"
+              :autoPlay="false"
+              color="success"
+            ></vuetify-audio>
+          </div>
+        </div>
+      </v-container>
+    </template>
+    <template v-if="outlineMenu">
+      <v-container v-for="book in bookDetail.data" :key="book.id_buku">
+        <v-row>
+          <v-col>
+            <v-list>
+              <v-list-item-group>
+                <v-list-item
+                  v-for="(item, i) in book.data_chapter"
+                  :key="item.id_chapter"
+                  @click="chapterArray = i, outlineMenu = false"
+                >
+                  <div style="width:100%;">
+                    <v-divider></v-divider>
+                    {{ i + 1 }}. {{ item.judul_chapter }}
+                    <v-divider></v-divider>
+                  </div>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 import { mapState } from "vuex";
 
 export default {
@@ -68,17 +193,93 @@ export default {
   data() {
     return {
       chapterArray: 0,
-      showTools: false
+      showTools: false,
+      showMenuTop: false,
+      outlineMenu: false,
+      min: 8,
+      max: 72,
+      slider: 24,
+      tooltipShow: false,
+      bgWhite: true,
+      bgGrey: false,
+      bgBlack: false
     };
   },
+  methods: {
+    hasHistory() {
+      return window.history?.length > 2;
+    }
+  },
   computed: mapState({
-    bookDetail: state => state.bookDetail
+    bookDetail: state => state.bookDetail,
+    styleObject() {
+      return {
+        fontSize: this.slider + "px"
+      };
+    }
   }),
   created() {
     this.$store.dispatch("getBookDetailByID", this.$route.params.bookId);
+  },
+  mounted() {
+    function getSelected() {
+      /* eslint-disable */
+      if (window.getSelection) {
+        return window.getSelection();
+      } else if (document.getSelection) {
+        return document.getSelection();
+      } else {
+        var selection = document.selection && document.selection.createRange();
+        if (selection.text) {
+          return selection.text;
+        }
+        return false;
+      }
+      return false;
+    }
+    document
+      .getElementById("chapterText")
+      .addEventListener("mouseup", function() {
+        var selection = getSelected();
+        if (selection) {
+          //   alert(selection);
+        //   var span = document.createElement("span");
+        //   span.innerHTML = window.getSelection().toString();
+        //   span.style.width = "80px";
+        //   span.style.border = "1px solid black";
+        //   span.style.backgroundColor = "#555";
+        //   span.style.position = "absolute";
+        //   span.style.zIndex = 1;
+        //   span.style.bottom = "125%";
+        //   span.style.left = "50%";
+        //   span.style.color = "#fff";
+        //   document.getElementById("chapterText").appendChild(span);
+        }
+      });
+    var tooltipSpan = document.getElementById("tooltip");
+
+    window.onmouseup = function(e) {
+      var x = e.clientX;
+	  var y = e.clientY;
+	  tooltipSpan.style.display = "flex";
+	  tooltipSpan.style.position = "fixed";
+	  tooltipSpan.style.overflow = "hidden";
+      tooltipSpan.style.top = y - 50 + "px";
+      tooltipSpan.style.left = x + "px";
+    };
   }
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+@import "@/assets/css/global_variables.scss";
+
+#chapterText #tooltip {
+  display: none;
+}
+.showTooltip{
+	display: block;
+	position: fixed;
+	overflow: hidden;
+}
 </style>
