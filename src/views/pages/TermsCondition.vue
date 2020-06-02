c0cc35c935e31d252933a1a2e3c5b9ae3376cce2
tyastomo <fahmityastomo@gmail.com>
2020-06-02 08:51:45 +0700

    terms

--- src/views/pages/TermsCondition.vue
+++ src/views/pages/TermsCondition.vue
@@ -5,26 +5,74 @@
 		<div class="container">
 		<v-row class="mt-2 my-2">
 			<v-col sm="12" md="12">
-				<h2>GENERAL TERMS AND CONDITIONS</h2>
-				<p> Dengan melakukan Transaksi Online di www.ahabaca.com, AHA iOS atau Android, Pembeli dianggap telah membaca dan menyetujui, dan oleh karena itu, terikat pada Syarat dan Ketentuan sebagaimana dijelaskan di bawah ini. </p>
+				<h2>Layanan dan Hak</h2>
+				<ol>
+					<li>Ahabaca memberikan wawasan utama dari buku-buku non-fiksi, artikel, dan konten lainnya (misalnya, "books-in-blinks" atau "articles-in-blinks", selanjutnya secara bersama-sama disebut sebagai "Konten Platform"), yang memungkinkan Anda untuk memahami esensi mereka dalam beberapa menit. Selain itu, Ahabaca dapat memberikan profil pribadi kepada Anda, yang dapat Anda isi dengan konten platform dan penanda teks pilihan Anda serta menyortir konten ini ke dalam berbagai kategori dan daftar. Selain itu, Anda dapat membeli buku audio dan konten audio premium lainnya ("Konten Buku Audio") di Platform.</li>
+					<li>Tingkat Konten platform dan opsi untuk digunakan ("Lisensi") tergantung pada jenis akun yang Anda pilih. Ada perbedaan antara akun gratis, langganan berbayar  dan uji coba gratis dan pembelian Konten Buku Audio. Dalam hal durasi, lisensi terbatas pada periode validitas akun gratis / langganan berbayar / uji coba gratis, namun, dalam kasus pembelian ketersediaan Konten Audiobook di akun Anda, tunduk pada Bagian 6, tidak terbatas.</li>
+					<li>Hak atas layanan Ahabaca dan konten yang didistribusikan melalui layanan adalah dan akan tetap menjadi milik Ahabaca dan / atau pemegang lisensi Ahabaca. Aplikasi dan konten perangkat lunak Ahabaca dilisensikan kepada Anda dalam lingkup penggunaan kontrak. Kami hanya memberi Anda hak non-eksklusif (sederhana), terbatas waktu untuk penggunaan pribadi layanan Ahabaca dan kontennya (namun, tunduk pada Bagian 6, ketersediaan Konten Audiobook yang dibeli tidak akan terbatas di akun Anda).</li>
+					<li>Semua merek dagang Ahabaca serta nama dagang, logo, nama domain kami, dan fitur khas lainnya dari merek dagang Ahabaca adalah milik tunggal Ahabaca. Lisensi tidak memberikan Anda hak untuk menggunakan merek dagang Ahabaca, nama dagang kami, logo, nama domain, atau fitur khas lainnya dari merek dagang Ahabaca, baik untuk tujuan komersial maupun non-komersial.</li>
+					<li>Jika Anda tidak menggunakan layanan berbayar, Ahabaca diizinkan untuk menunjukkan kepada Anda informasi promosi di Platform.</li>
+				</ol>
 			</v-col>
 		</v-row>
 		<v-row class="mt-2 my-2">
 			<v-col sm="12" md="12">
-				<h2>METODE DAN ATURAN PEMBAYARAN</h2>
-				<p> Metode Pembayaran yang tersedia di AHABACA adalah dengan menggunakan</p>
-				<ul>
+				<h2>Mendaftarkan Diri di AHABACA</h2>
+				<ol>
 					<li>
-						Transfer Bank
+						Agar dapat menggunakan layanan Ahabaca, diperlukan pendaftaran dengan Ahabaca. Orang yang berusia di atas 18 tahun berhak untuk mendaftar. Sebagai anak di bawah umur, Anda hanya dapat mendaftar dengan Ahabaca jika Anda berusia setidaknya 14 tahun dan perwakilan hukum Anda telah memberikan persetujuan mereka. Ahabaca berhak untuk menggunakan layanan Ahabaca bergantung pada bukti yang tepat dari identitas Anda, usia hukum Anda, atau persetujuan perwakilan hukum Anda.
 					</li>
 					<li>
-						Virtual Account
+						Registrasi itu sendiri tidak dikenai biaya (akun gratis) dan tidak mengharuskan Anda untuk berlangganan. Dengan mendaftar, Anda menjalankan perjanjian dengan Ahabaca untuk penggunaan Platform yang terbatas dan gratis.
 					</li>
 					<li>
-						Credit Card (Visa/Mastercard
+						Anda dapat menggunakan alamat email Anda dan juga kata sandi pilihan Anda untuk mendaftar. Atau, Anda dapat mendaftar menggunakan layanan masuk tunggal (misalnya, login Facebook). Anda dapat mendaftar melalui situs web kami atau setelah mengunduh aplikasi iOS, Android, atau Kindle Fire kami.
 					</li>
-				</ul> <br/>
-				<p>Untuk pembayaran menggunakan Credit Card (Visa/Master Card), biaya ditanggung oleh Pembeli.</p>
+					<li>
+						Setiap informasi yang diperlukan untuk registrasi harus lengkap dan benar dan selalu terbaru. Ahabaca memiliki hak untuk menyimpan dan memproses data yang Anda berikan saat pendaftaran, sesuai dengan ketentuan tentang Perlindungan Data.
+					</li>
+					<li>
+						Kata sandi yang digunakan harus dijaga kerahasiaannya. Menjaga kerahasiaannya adalah sepenuhnya dan sepenuhnya tanggung jawab Anda. Akun pengguna Ahabaca hanya dapat digunakan oleh Anda. Setiap penggunaan yang tidak sah dari akun pengguna Anda, serta segala kecurigaan terhadap efek tersebut harus segera dikomunikasikan ke Ahabaca.
+					</li>
+				</ol>
+			</v-col>
+		</v-row>
+		<v-row class="mt-2 my-2">
+			<v-col sm="12" md="12">
+				<h2>Melaksanakan Perjanjian untuk Berlangganan Berbayar</h2>
+				<ol>
+					<li>
+						Agar dapat sepenuhnya memanfaatkan layanan Ahabaca, paket layanan tambahan dalam bentuk langganan berbayar ("Akses Langganan") tersedia untuk Anda setelah mendaftar. Ini dapat dibeli untuk periode waktu yang berbeda dan variabel, yang akan Anda lihat dalam opsi pemesanan.
+					</li>
+					<li>
+						Akses Langganan Ahabaca adalah langganan bebas iklan dan berbayar yang memungkinkan Anda untuk mengakses konten dan fitur tambahan melalui aplikasi seluler dan berbasis web kami. Anda akan memiliki akses ke layanan masing-masing selama langganan Anda valid.
+					</li>
+					<li>
+						Anda mengirimkan penawaran yang mengikat secara hukum untuk berlangganan berbasis biaya, jika Anda memasukkan informasi yang diminta dalam formulir pemesanan online dan kemudian mengklik tombol "Pesan dengan kewajiban membayar". Dengan melakukan itu, Anda mengajukan penawaran yang mengikat secara hukum untuk perjanjian langganan berbayar. Sebelum mengirimkan pesanan Anda, Anda dapat mengubah dan melihat data kapan saja.
+					</li>
+					<li>
+						Kontrak berakhir ketika Ahabaca mengeluarkan pemberitahuan penerimaan (konfirmasi pesanan), yang akan dikirimkan kepada Anda melalui email dalam waktu yang wajar. Jika Anda membeli langganan berbasis biaya melalui aplikasi berbasis web Ahabaca, Ahabaca akan mengirimkan konfirmasi pesanan kepada Anda melalui email bersama dengan konfirmasi tanda terima segera setelah pesanan Anda. Jika Anda membeli langganan berbasis biaya melalui pembelian dalam aplikasi (harap lihat juga ayat 5), Anda akan menerima konfirmasi tanda terima mengikuti pesanan Anda dari toko pihak ketiga dan Ahabaca akan mengirimkan kepada Anda pemberitahuan penerimaan (konfirmasi pesanan) oleh email terpisah. Dalam kedua kasus tersebut, teks kontrak (terdiri dari pesanan, Syarat dan Ketentuan Umum, dan konfirmasi pesanan) akan dikirimkan kepada Anda oleh Ahabaca pada penyedia data permanen (email) (konfirmasi kontrak). Langganan Anda dimulai pada tanggal ini. Teks perjanjian disimpan tunduk pada perlindungan data. Silakan baca Pernyataan Privasi kami untuk informasi tentang pengumpulan, penggunaan, dan pemrosesan data Anda.
+					</li>
+					<li>
+						Anda juga dapat mendaftar untuk berlangganan berbayar melalui pembelian dalam aplikasi melalui aplikasi iOS atau Android kami. Untuk melakukan ini, Anda harus memilih opsi yang diinginkan pada layar berlangganan di dalam aplikasi, dan sembulan akan muncul di mana Anda harus memasukkan kata sandi Anda untuk toko aplikasi pihak ketiga. Segera setelah Anda selesai, pop-up akan muncul di mana Anda harus mengkonfirmasi pembelian berlangganan lagi. Pada titik ini Anda masih dapat membatalkan proses. Anda kemudian harus menerima konfirmasi penerimaan dari toko pihak ketiga (tidak langsung dari Ahabaca).
+					</li>
+				</ol>
+			</v-col>
+		</v-row>
+		<v-row class="mt-2 my-2">
+			<v-col sm="12" md="12">
+				<h2>Uji Coba / Fase Pengujian</h2>
+				<ol>
+					<li>
+						Ahabaca sesekali menawarkan secara khusus kepada pengguna baru, Akses Langganan gratis untuk tujuan pengujian selama periode waktu tertentu ("Uji Coba"). Ahabaca menentukan, atas kebijakannya sendiri, jika Anda dapat berpartisipasi dalam Percobaan. Jika ada alasan penting, Ahabaca dapat menghentikan atau mengubah Uji coba gratis setiap saat tanpa pemberitahuan atau pemberitahuan sebelumnya.
+					</li>
+					<li>
+						Anda hanya dapat berpartisipasi dalam Uji Coba tertentu jika Anda memasukkan rincian pembayaran saat mendaftar untuk Uji Coba. Dalam kasus seperti itu, kami memerlukan persetujuan Anda di awal Uji Coba bahwa akses gratis Anda akan dikonversi menjadi Akses Berlangganan berbayar pada hari setelah berakhirnya Uji Coba. Dalam situasi ini, langganan Anda memperbarui dirinya secara berulang dan dapat dihentikan hingga satu hari sebelum akhir periode berlangganan masing-masing.
+					</li>
+					<li>
+						Jika Anda tidak ingin memiliki Akses Langganan dan biaya yang terkait dengannya, Anda harus membatalkan Akses Langganan yang diberikan kepada Anda selama Percobaan sebelum akhir Percobaan (misalnya, dengan hanya pergi ke Pengaturan Akun Ahabaca Anda atau pengaturan toko aplikasi masing-masing).
+					</li>
+				</ol>
 			</v-col>
 		</v-row>
 		<v-row class="mt-2 my-2">
