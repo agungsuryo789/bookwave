4cd0981ac76cdc07a16725c17cf116ffecae4d29
Agung Suryo Sundoro <agungsuryo645@gmail.com>
2020-06-05 11:04:23 +0700

    Fix Episode Card Href and style

--- src/components/EpisodeCard.vue
+++ src/components/EpisodeCard.vue
@@ -1,27 +1,25 @@
 <template>
-  <div>
-    <router-link tag="p" :to="{ name: 'BookChapter', params: {bookId: idBuku, chapterId: idChapter}}">
-      <v-card
-        class="episode-card mx-auto"
-        max-width="350"
-        max-height="250px"
-        ripple
-        :style="cssVars"
-      >
-        <div class="d-flex flex-row justify-space-between align-center">
-          <div class="mx-3">
-            <p>Baru</p>
-            <p>{{ episodeTitle }}</p>
-            <small>Tema Episode</small>
-          </div>
-          <div class="mx-3 episode-card-img-color">
-            <div>
-              <v-img class="book-card-img mx-auto" width="40" height="40" :src="foto_sampul"></v-img>
-            </div>
+  <div class="div--episode-card" :style="cssVars">
+    <v-card
+      class="episode-card mx-auto"
+      max-width="350"
+      max-height="250px"
+      ripple
+      :to="{ name: 'BookChapter', params: {bookId: idBuku, chapterId: idChapter}}"
+    >
+      <div class="d-flex flex-row justify-space-between align-center">
+        <div class="mx-3">
+          <p>Baru</p>
+          <p>{{ episodeTitle }}</p>
+          <small>Tema Episode</small>
+        </div>
+        <div class="mx-3 episode-card-img-color">
+          <div>
+            <v-img class="book-card-img mx-auto" width="40" height="40" :src="foto_sampul"></v-img>
           </div>
         </div>
-      </v-card>
-    </router-link>
+      </div>
+    </v-card>
   </div>
 </template>
 
@@ -47,8 +45,12 @@ export default {
 </script>
 
 <style lang="scss">
+.div--episode-card {
+  border: 4px solid var(--color);
+  border-bottom: 8px solid var(--color);
+  border-radius: 6px;
+}
 .episode-card {
-  background-color: red;
   .episode-card-img-color {
     border-radius: 8px;
     padding: 9px;
