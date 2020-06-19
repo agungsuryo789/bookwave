1feaeb2d28fb85a59aaeea526cf0cbb3453b971a
Agung Suryo Sundoro <agungsuryo645@gmail.com>
2020-06-19 09:19:44 +0700

    Fix Mobile Navbar
    Fix Mobile Navbar
    Fix Mobile view Subscription Page
    Fix UI Book Card
    Fix UI Payment History

--- src/views/product/MembershipPage.vue
+++ src/views/product/MembershipPage.vue
@@ -12,41 +12,45 @@
       <div class="d-flex flex-row justify-center align-center my-12">
         <h2>UPGRADE KE AHA PREMIUM</h2>
       </div>
-      <v-row>
-        <v-col lg="4" md="6" sm="12" xs="12">
-          <ul>
-            <li>Simpan lebih dari 256 buku bermanfaat</li>
-            <li>Baca pada semua perangkat</li>
-            <li>Putar bacaan buku tanpa halangan</li>
-            <li>Tandai dan simpan kalimat penting</li>
-          </ul>
-        </v-col>
-        <v-col lg="4" md="6" sm="12" xs="12" v-for="item in subList" :key="item.id_langganan">
-          <v-card height="180px">
-            <div class="d-flex flex-row justify-space-between pt-2">
-              <div
-                v-if="item.id_langganan == 2"
-                class="subcription-page--price-badge justify-center align-center"
-                style="background-color:#B2FF59;height:35px;padding:3px;color:white;"
-              >
-                <p>Hemat 20%</p>
+      <div class="d-flex flex-column flex-lg-row flex-xl-row">
+        <v-row>
+          <v-col lg="12" md="6" sm="12" xs="12">
+            <ul>
+              <li>Simpan lebih dari 256 buku bermanfaat</li>
+              <li>Baca pada semua perangkat</li>
+              <li>Putar bacaan buku tanpa halangan</li>
+              <li>Tandai dan simpan kalimat penting</li>
+            </ul>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col lg="6" md="6" sm="12" xs="12" v-for="item in subList" :key="item.id_langganan">
+            <v-card height="180px">
+              <div class="d-flex flex-row justify-space-between pt-2">
+                <div
+                  v-if="item.id_langganan == 2"
+                  class="subcription-page--price-badge justify-center align-center"
+                  style="background-color:#B2FF59;height:35px;padding:3px;color:white;"
+                >
+                  <p>Hemat 20%</p>
+                </div>
+                <div v-else></div>
+                <v-btn
+                  color="#D32F2F"
+                  elevation="3"
+                  ripple
+                  style="color:white;font-weight:bold;"
+                  @click="pay(item.id_langganan)"
+                >Coba 7 hari gratis</v-btn>
               </div>
-              <div v-else></div>
-              <v-btn
-                color="#D32F2F"
-                elevation="3"
-                ripple
-                style="color:white;font-weight:bold;"
-                @click="pay(item.id_langganan)"
-              >Coba 7 hari gratis</v-btn>
-            </div>
-            <v-card-title class="m-0 p-0">{{ item.nama_langganan }}</v-card-title>
-            <v-card-text class="m-0 p-0">
-              <h2>{{ item.harga }} / Bulan</h2>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+              <v-card-title class="m-0 p-0">{{ item.nama_langganan }}</v-card-title>
+              <v-card-text class="m-0 p-0">
+                <h2>{{ item.harga }} / Bulan</h2>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
     </v-container>
     <FooterSection />
   </div>
