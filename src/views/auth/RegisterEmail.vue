169add745a837de15dd18f05ffeb82ce10e65903
Agung Suryo Sundoro <agungsuryo645@gmail.com>
2020-06-23 12:03:01 +0700

    Fix Register/Login Email 422 resp
    Fix Register/Login Email 422 resp
    Fix json structure login/register
    Fix Auth mutation response

--- src/views/auth/RegisterEmail.vue
+++ src/views/auth/RegisterEmail.vue
@@ -1,128 +1,166 @@
 <template>
-<div class="Login">
-    <v-app>
-        <NavbarSection />
-		<v-form>
-        <v-container fluid>
-            <v-row>
-                <v-col cols="12" class="mt-12">
-					<v-row justify="center" class="mt-12">
-                        <v-col cols="6" md="4" align="center">
-							<h2 class="font-weight-black"> DAFTAR AKUN </h2>
-                        </v-col>
-                    </v-row>
-                </v-col>
-				<v-col v-show="isShow" class="mt-8" cols="12">
-					<v-row justify="center">
-						<v-col cols="10" md="4" align="center">
-							<p class="red--text text--lighten-1"><v-icon class="red--text text--lighten-1 mr-3">mdi-email-outline</v-icon>Alamat Email</p>
-							<v-text-field v-model="email" class="centered-input" :error-messages="emailErrors" required @input="$v.email.$touch()" @blur="$v.email.$touch()" solo background-color="grey lighten-2"></v-text-field>
-						</v-col>
-					</v-row>
-					<v-row justify="center">
-						<v-col cols="3" md="1" align="center">
-							<v-btn :elevation="8" color="red darken-1" block x-large class="white--text" v-on:click="lanjut">Lanjut</v-btn>
-						</v-col>
-					</v-row>
-				</v-col>
-				<v-col v-show="!isShow" class="mt-8" cols="12">
-					<v-row justify="center">
-						<v-col cols="6" md="4" align="center">
-							<p class="red--text text--lighten-1"><v-icon class="red--text text--lighten-1 mr-3">mdi-lock-outline</v-icon>Kata Sandi</p>
-							<v-text-field class="centered-input" v-model="password" :error-messages="passwordErrors" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"  :type="show1 ? 'text' :'password'" counter @click:append="show1 = !show1" @input="$v.password.$touch()" @blur="$v.password.$touch()" solo background-color="grey lighten-2"></v-text-field>
-						</v-col>
-					</v-row>
-					<v-row justify="center">
-						<v-col cols="10" md="1" align="center">
-							<v-btn :elevation="8" color="red darken-1" block x-large class="white--text" @click="submit">Daftar</v-btn>
-						</v-col>
-					</v-row>
-				</v-col>
+  <div class="Login">
+    <NavbarSection />
+    <v-form>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" class="mt-12">
+            <v-row justify="center" class="mt-12">
+              <v-col cols="6" md="4" align="center">
+                <h2 class="font-weight-black">DAFTAR AKUN</h2>
+              </v-col>
             </v-row>
-			<v-spacer></v-spacer>
-			<v-row justify="center">
-				<v-col cols="6" md="4" align="center" class="mt-12">
-					<router-link to="/login">Masuk</router-link>
-				</v-col>
-			</v-row>
-        </v-container>
-		</v-form>
-    </v-app>
-</div>
+          </v-col>
+          <v-col v-show="isShow" class="mt-8" cols="12">
+            <v-row justify="center">
+              <v-col cols="10" md="4" align="center">
+                <p class="red--text text--lighten-1">
+                  <v-icon class="red--text text--lighten-1 mr-3">mdi-email-outline</v-icon>Alamat Email
+                </p>
+                <v-text-field
+                  v-model="registerPayload.email"
+                  class="centered-input"
+                  :error-messages="emailErrors"
+                  required
+                  @input="$v.registerPayload.email.$touch()"
+                  @blur="$v.registerPayload.email.$touch()"
+                  solo
+                  background-color="grey lighten-2"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row justify="center">
+              <v-col cols="3" md="1" align="center">
+                <v-btn
+                  :elevation="8"
+                  color="red darken-1"
+                  block
+                  x-large
+                  class="white--text"
+                  v-on:click="lanjut"
+                >Lanjut</v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col v-show="!isShow" class="mt-8" cols="12">
+            <v-row justify="center">
+              <v-col cols="6" md="4" align="center">
+                <p class="red--text text--lighten-1">
+                  <v-icon class="red--text text--lighten-1 mr-3">mdi-lock-outline</v-icon>Kata Sandi
+                </p>
+                <v-text-field
+                  class="centered-input"
+                  v-model="registerPayload.password"
+                  :error-messages="passwordErrors"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="show1 ? 'text' :'password'"
+                  counter
+                  @click:append="show1 = !show1"
+                  @input="$v.registerPayload.password.$touch()"
+                  @blur="$v.registerPayload.password.$touch()"
+                  solo
+                  background-color="grey lighten-2"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row justify="center">
+              <v-col cols="10" md="1" align="center">
+                <v-btn
+                  :elevation="8"
+                  color="red darken-1"
+                  block
+                  x-large
+                  class="white--text"
+                  @click="submit"
+                >Daftar</v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-spacer></v-spacer>
+        <v-row justify="center">
+          <v-col cols="6" md="4" align="center" class="mt-12">
+            <router-link to="/login">Masuk</router-link>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
 <script>
-import NavbarSection from '@/components/NavbarSection.vue'
-import { validationMixin } from 'vuelidate'
-import { required, email, minLength } from 'vuelidate/lib/validators'
+import NavbarSection from "@/components/NavbarSection.vue";
+import { validationMixin } from "vuelidate";
+import { required, email, minLength } from "vuelidate/lib/validators";
 
 /* eslint-disable */
 export default {
-	name: 'RegisterEmail',
-	mixins: [validationMixin],
-	validations: {
-		email: { required, email },
-		password: {
-			required,
-			minLength: minLength(8)
-		}
+  name: "RegisterEmail",
+  mixins: [validationMixin],
+  validations: {
+    registerPayload: {
+      email: { required, email },
+      password: {
+        required,
+        minLength: minLength(8)
+      }
+    }
+  },
+  components: {
+    NavbarSection
+  },
+  data() {
+    return {
+      isShow: true,
+      lazy: false,
+      show1: false,
+      registerPayload: {
+        email: '',
+        password: '',
+        type: "1"
+      }
+    }
+  },
+  computed: {
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.registerPayload.email.$dirty) return errors;
+      !this.$v.registerPayload.email.email && errors.push("Must be valid e-mail");
+      !this.$v.registerPayload.email.required && errors.push("E-mail is required");
+      return errors;
     },
-    components: {
-		NavbarSection
-	},
-	data(){
-		return {
-			isShow : true,
-			email: '',
-			lazy: false,
-			show1: false,
-			password: ''
-		}
-	},
-	computed: {
-		emailErrors () {
-			const errors = []
-			if (!this.$v.email.$dirty) return errors
-			!this.$v.email.email && errors.push('Must be valid e-mail')
-			!this.$v.email.required && errors.push('E-mail is required')
-			return errors
-		  },
-		passwordErrors () {
-			const errors = []
-			if (!this.$v.password.$dirty) return errors
-			!this.$v.password.minLength && errors.push('At least must be 8 character')
-			!this.$v.password.required && errors.push('Password is required')
-			return errors
-		}
-	},
-	methods: {
-		lanjut () {
-			this.$v.email.$touch()
-			if (this.$v.email.$invalid) {
-				this.isShow = true
-			} else {
-				this.isShow = false
-			}
-		},
-      	submit () {
-			this.$v.password.$touch()
-			if (this.$v.password.$invalid) {
-				this.snackbar = true
-			} else {
-				var data = {
-					email: this.email,
-					password: this.password,
-					type: 1
-				}
-				this.$store.dispatch('userRegister', data)
-			}
-      	}
-	}
-}
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.registerPayload.password.$dirty) return errors;
+      !this.$v.registerPayload.password.minLength &&
+        errors.push("At least must be 8 character");
+      !this.$v.registerPayload.password.required && errors.push("Password is required");
+      return errors;
+    }
+  },
+  methods: {
+    lanjut() {
+      this.$v.registerPayload.email.$touch();
+      if (this.$v.registerPayload.email.$invalid) {
+        this.isShow = true;
+      } else {
+        this.isShow = false;
+      }
+    },
+    submit() {
+      this.$v.registerPayload.password.$touch();
+      if (this.$v.registerPayload.password.$invalid) {
+        this.snackbar = true;
+      } else {
+        this.$store.dispatch("userRegister", this.registerPayload);
+      }
+    }
+  }
+};
 </script>
 
 <style>
-	.centered-input input {
-  		text-align: center
-	}
+.centered-input input {
+  text-align: center;
+}
 </style>
